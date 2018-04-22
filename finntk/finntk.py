75d7d11bec90abf148d83c1070ee0961266216ce
Frankie Robertson <frankie@robertson.name>
2018-04-22 18:06:23 +0300

    Add some simple OMorFi output processing tools

--- finntk/finntk.py
+++ finntk/finntk.py
@@ -1,12 +1,72 @@
+import re
 from omorfi.omorfi import Omorfi
+from more_itertools import split_at
+
+
+def analysis_to_pairs(ana):
+    for bit in ana.split(']['):
+        k, v = bit.strip('[]').split('=', 1)
+        yield k, v
+
+
+def pairs_to_dict(it):
+    return dict(((k.lower(), v) for k, v in it))
 
 
 def analysis_to_dict(ana):
-    d = {}
-    for bit in ana.split(']['):
-        k, v = bit.strip('[]').split('=', 1)
-        d[k.lower()] = v
-    return d
+    return pairs_to_dict(analysis_to_pairs(ana))
+
+
+def chunk_subwords(it):
+    def is_cmp_bound(kv):
+        return (kv[0] == 'BOUNDARY' and
+                kv[1] == 'COMPOUND')
+
+    return split_at(it, is_cmp_bound)
+
+
+def analysis_to_subword_dicts(ana):
+    return map(pairs_to_dict, chunk_subwords(analysis_to_pairs(ana)))
+
+
+LABELSEGMENT_RE = r'''
+    \{ (?P<seg> [^\}]* ) \} |
+    \[ (?P<tag> [^\]]* ) \] |
+    (?P<surf> [^\[\{]+ )
+'''
+
+_labelsegment_lex = None
+
+
+def get_labelsegment_lex():
+    global _labelsegment_lex
+    if _labelsegment_lex is None:
+        _labelsegment_lex = re.compile(LABELSEGMENT_RE, re.VERBOSE)
+    return _labelsegment_lex
+
+
+def labelsegment_to_tokens(labelsegmented):
+    lex = get_labelsegment_lex()
+    for match in re.finditer(lex, labelsegmented):
+        typ = match.lastgroup
+        value = match.group(typ)
+        yield typ, value
+
+
+def tokens_to_subword_tokens(it):
+    def is_cmp_bound(kv):
+        return (kv[0] == 'seg' and
+                kv[1] == 'wB')
+
+    return split_at(it, is_cmp_bound)
+
+
+def tokens_to_surf(it):
+    return "".join(v for (t, v) in it if t == 'surf')
+
+
+def labelsegment_to_subword_tokens(labelsegmented):
+    return tokens_to_subword_tokens(labelsegment_to_tokens(labelsegmented))
 
 
 _omorfi = None
--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setuptools.setup(
 
     packages=setuptools.find_packages(),
 
-    install_requires=[],
+    install_requires=[
+        "more_itertools>=4.1.0"
+    ],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
