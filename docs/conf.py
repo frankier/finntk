a4253a9bd3cb79ec9d3f1c09d4ece24cfe9698af
Frankie Robertson <frankie@robertson.name>
2018-06-01 13:28:02 +0300

    Mock omorfi to build docs

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ extensions = [
 
 autosummary_generate = True
 
-autodoc_mock_imports = ["ahocorasick", "more_itertools"]
+autodoc_mock_imports = ["ahocorasick", "more_itertools", "omorfi"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
