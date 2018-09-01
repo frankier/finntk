34d0e62e2a509ef359ba6a4f0d1b5a4cfff9ed77
Frankie Robertson <frankie@robertson.name>
2018-09-01 23:27:14 +0300

    Fix install-finnpos.sh so it installs the ftb-omorfi tagger

--- docker/install-finnpos.sh
+++ docker/install-finnpos.sh
@@ -2,4 +2,9 @@ git clone https://github.com/mpsilfve/FinnPos/
 cd FinnPos
 git checkout 82bce745afc658444fab1bb58b47bff89c207ba2
 make
+wget https://github.com/mpsilfve/FinnPos/releases/download/v0.1-alpha/morphology.omor.hfst.gz
+gunzip morphology.omor.hfst.gz
+mv morphology.omor.hfst share/finnpos/omorfi/morphology.omor.hfst
+make ftb-omorfi-tagger
 make install
+make install-models
