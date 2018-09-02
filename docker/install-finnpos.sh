adb02e82b1ea43ab685bd573bebef2df048cc270
Frankie Robertson <frankie@robertson.name>
2018-09-02 13:25:29 +0300

    Download ftb_omorfi_model rather than trying to build it on Docker Hub

--- docker/install-finnpos.sh
+++ docker/install-finnpos.sh
@@ -5,6 +5,7 @@ make
 wget https://github.com/mpsilfve/FinnPos/releases/download/v0.1-alpha/morphology.omor.hfst.gz
 gunzip morphology.omor.hfst.gz
 mv morphology.omor.hfst share/finnpos/omorfi/morphology.omor.hfst
-make ftb-omorfi-tagger
+wget https://github.com/frankier/finntk/releases/download/bins/ftb_omorfi_model.zip
+unzip ftb_omorfi_model.zip
 make install
 make install-models
