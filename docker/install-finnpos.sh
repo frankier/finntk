d2d884ecc9c8d2113c6faeb78edbaa790e0c897a
Frankie Robertson <frankie@robertson.name>
2018-09-02 23:29:20 +0300

    Patch FinnPOS makefile to prevent model training

--- docker/install-finnpos.sh
+++ docker/install-finnpos.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/mpsilfve/FinnPos/
 cd FinnPos
 git checkout 82bce745afc658444fab1bb58b47bff89c207ba2
+cp ../finnpos-makefile.patch .
+patch Makefile finnpos-makefile.patch
 make
 wget https://github.com/mpsilfve/FinnPos/releases/download/v0.1-alpha/morphology.omor.hfst.gz
 gunzip morphology.omor.hfst.gz
