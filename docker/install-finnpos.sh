331f471004d1318bfe2382d96b59edbd026ef785
Frankie Robertson <frankie@robertson.name>
2018-09-07 08:59:15 +0300

    Adjust order of items in Makefile

--- docker/install-finnpos.sh
+++ docker/install-finnpos.sh
@@ -4,11 +4,12 @@ cd FinnPos
 git checkout 82bce745afc658444fab1bb58b47bff89c207ba2
 cp ../finnpos-makefile.patch .
 patch Makefile finnpos-makefile.patch
-make
 wget https://github.com/mpsilfve/FinnPos/releases/download/v0.1-alpha/morphology.omor.hfst.gz
 gunzip morphology.omor.hfst.gz
 mv morphology.omor.hfst share/finnpos/omorfi/morphology.omor.hfst
 wget https://github.com/frankier/finntk/releases/download/bins/ftb_omorfi_model.zip
 unzip ftb_omorfi_model.zip
+make
+make ftb-omorfi-tagger
 make install
 make install-models
