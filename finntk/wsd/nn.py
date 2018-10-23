69d4b3a2d36cee4c919f449c055d86b4be9a4eb0
Frankie Robertson <frankie@robertson.name>
2018-10-23 20:16:09 +0300

    Replace WsdNn with WordExpert/WordExpertManager

--- finntk/wsd/nn.py
+++ finntk/wsd/nn.py
@@ -1,33 +1,56 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from os import makedirs
+from os.path import join as pjoin, exists
+import pickle
 
 
-class WsdNn:
+class WordExpert:
 
     def __init__(self):
-        self.classifiers = {}
-        self.xs = {}
-        self.ys = {}
+        self.xs = []
+        self.ys = []
+        self.clf = None
 
-    def add_word(self, word, ctx_vec, sense_key):
+    def add_word(self, ctx_vec, sense_key):
         vec_norm = np.linalg.norm(ctx_vec)
-        self.xs.setdefault(word, []).append(ctx_vec / vec_norm)
-        self.ys.setdefault(word, []).append(sense_key)
+        self.xs.append(ctx_vec / vec_norm)
+        self.ys.append(sense_key)
 
-    def fit_word(self, word):
-        if word not in self.xs or word not in self.ys:
-            return
+    def fit(self):
         clf = NearestNeighbors(n_neighbors=1, algorithm="ball_tree")
-        clf.fit(self.xs[word], self.ys[word])
-        self.classifiers[word] = clf
-        del self.xs[word]
-
-    def fit_all(self):
-        for word in self.xs.keys():
-            self.fit_word(word)
+        clf.fit(self.xs, self.ys)
+        self.clf = clf
         del self.xs
 
-    def predict(self, word, ctx_vec):
-        indices = self.classifiers[word].kneighbors([ctx_vec], return_distance=False)
+    def predict(self, ctx_vec):
+        indices = self.clf.kneighbors([ctx_vec], return_distance=False)
         index = indices[0, 0]
-        return self.ys[word][index]
+        return self.ys[index]
+
+
+class ExpertExists(Exception):
+    pass
+
+
+class WordExpertManager:
+
+    def __init__(self, path, mode="r", allow_overwrites=False):
+        self.path = path
+        self.allow_overwrites = allow_overwrites
+        if mode == "w":
+            makedirs(path, exist_ok=allow_overwrites)
+
+    def _expert_path(self, word):
+        return pjoin(self.path, word)
+
+    def dump_expert(self, word, expert):
+        expert_path = self._expert_path(word)
+        if not self.allow_overwrites and exists(expert_path):
+            raise ExpertExists(f"Not overwriting existing word expert: {expert_path}")
+        with open(expert_path, "wb") as outf:
+            pickle.dump(expert, outf)
+
+    def load_expert(self, word):
+        with open(self._expert_path(word), "rb") as inf:
+            return pickle.load(inf)
