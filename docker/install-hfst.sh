81d61b9cc2a00e26839505baba386149982c0d3c
Frankie Robertson <frankie@robertson.name>
2019-01-23 12:11:32 +0200

    Add Debian based Dockerfile

--- docker/install-hfst.sh
+++ docker/install-hfst.sh
@@ -6,3 +6,8 @@ git checkout 683a5f4e8d457bfa5d25014b7a4346d77427ee20
 ./configure --enable-all-tools --with-unicode-handler=glib
 make
 make install
+if [ "$1" ]; then
+  cd python
+  python3 setup.py install
+  python3 setup.py install
+fi
