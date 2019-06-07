62e17b44d98ee718b24688af47a4244917409b0c
Frankie Robertson <frankie@robertson.name>
2019-06-07 17:43:42 +0300

    Version bump => 0.0.40

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="finntk",
-    version="0.0.39",
+    version="0.0.40",
     url="https://github.com/frankier/finntk",
     author="Frankie Robertson",
     description="Finnish NLP toolkit",
