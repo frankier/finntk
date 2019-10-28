2129ccdb09710f4e449064a474715e317b90c018
Frankie Robertson <frankie@robertson.name>
2019-10-28 16:13:18 +0200

    Add WordNet util: maybe_fi2en_ss

--- finntk/wordnet/utils.py
+++ finntk/wordnet/utils.py
@@ -1,3 +1,7 @@
+from nltk.corpus.reader.wordnet import Synset, WordNetError
+from typing import Optional
+
+
 def pre_id_to_post(pre_id):
     """
     Convert a synset id of the format n88888888 to 88888888-n
@@ -38,3 +42,19 @@ def en2fi_post(post_id):
 
     fi2en, en2fi = get_en_fi_maps()
     return pre_id_to_post(en2fi[post_id_to_pre(post_id, conv_s=True)])
+
+
+def maybe_fi2en_ss(ss: Synset) -> Optional[Synset]:
+    from .reader import get_en_fi_maps
+    from .utils import ss2pre, pre2ss
+    from nltk.corpus import wordnet
+
+    fi2en, _en2fi = get_en_fi_maps()
+    pre_fi = ss2pre(ss)
+    pre_en = fi2en.get(pre_fi)
+    if pre_en is None:
+        return None
+    try:
+        return pre2ss(wordnet, pre_en)
+    except WordNetError:
+        return None
