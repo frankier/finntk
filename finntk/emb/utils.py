12c90b98fe43286782dca6c6c0ec5b93e0b2440c
Frankie Robertson <frankie@robertson.name>
2019-11-03 16:06:09 +0200

    Modify embedding means to take a dtype parameter

--- finntk/emb/utils.py
+++ finntk/emb/utils.py
@@ -5,23 +5,25 @@ from sklearn.decomposition import TruncatedSVD
 from numpy.ma import size
 
 
-def power_mean_inner(p, mat, axis=0):
+def power_mean_inner(p, mat, axis=0, dtype=None):
     mat_cmplx = mat.astype(np.complex128)
     result_cmplx = np.power(
-        (np.power(mat_cmplx, p)).sum(axis=axis) / size(mat_cmplx, axis=axis), 1 / p
+        (np.power(mat_cmplx, p)).sum(axis=axis, dtype=dtype)
+        / size(mat_cmplx, axis=axis),
+        1 / p,
     )
     return result_cmplx.real.astype(np.float_)
 
 
-def power_mean(p, mat):
+def power_mean(p, mat, dtype=None):
     if p == float("inf"):
-        return mat.max(axis=0)
+        return mat.max(axis=0, dtype=dtype)
     elif p == float("-inf"):
-        return mat.min(axis=0)
+        return mat.min(axis=0, dtype=dtype)
     elif p == 1:
-        return mat.mean(axis=0)
+        return mat.mean(axis=0, dtype=dtype)
     else:
-        return power_mean_inner(p, mat, axis=0)
+        return power_mean_inner(p, mat, axis=0, dtype=dtype)
 
 
 def bow_to_mat(space, stream):
@@ -42,7 +44,7 @@ CATP_3 = (float("-inf"), 1, float("inf"))
 CATP_4 = (float("-inf"), 1, 3, float("inf"))
 
 
-def catp_mean(mat, ps=CATP_3):
+def catp_mean(mat, ps=CATP_3, dtype=None):
     """
     From *Concatenated p-mean Word Embeddings as Universal Cross-Lingual
     Sentence Representations*
@@ -50,7 +52,7 @@ def catp_mean(mat, ps=CATP_3):
     """
     p_means = []
     for p in ps:
-        p_means.append(power_mean(p, mat))
+        p_means.append(power_mean(p, mat, dtype=dtype))
     return np.hstack(p_means)
 
 
@@ -60,7 +62,7 @@ def compute_pc(X):
     return svd.components_
 
 
-def pre_sif_mean_inner(mat, freqs, a):
+def pre_sif_mean_inner(mat, freqs, a, dtype=None):
     """
     From *A Simple but Tough-to-Beat Baseline for Sentence Embeddings*
     https://openreview.net/forum?id=SyK00v5xx
@@ -72,7 +74,7 @@ def pre_sif_mean_inner(mat, freqs, a):
     rows, cols = mat.shape
     for coli, freq in enumerate(freqs):
         mat[coli, :] = (a / (a + freq)) * mat[coli, :]
-    return mat.mean(axis=0)
+    return mat.mean(axis=0, dtype=dtype)
 
 
 def get_wf(maybe_pair):
@@ -81,9 +83,12 @@ def get_wf(maybe_pair):
     return maybe_pair
 
 
-def pre_sif_mean(mat, refs, lang):
+def pre_sif_mean(mat, refs, lang, dtype=None):
     return pre_sif_mean_inner(
-        mat, (wordfreq.word_frequency(get_wf(ref), lang) for ref in refs), 1e-3
+        mat,
+        (wordfreq.word_frequency(get_wf(ref), lang) for ref in refs),
+        1e-3,
+        dtype=dtype,
     )
 
 
@@ -92,21 +97,21 @@ pre_sif_mean.needs_words = True
 
 def mk_sif_mean(pc):
 
-    def sif_mean(mat, refs, lang):
-        emb = pre_sif_mean(mat, refs, lang)
+    def sif_mean(mat, refs, lang, dtype=None):
+        emb = pre_sif_mean(mat, refs, lang, dtype=dtype)
         return emb - emb.dot(pc) * pc
 
     sif_mean.needs_words = True
     return sif_mean
 
 
-def unnormalized_mean(mat):
-    return mat.mean(axis=0)
+def unnormalized_mean(mat, dtype=None):
+    return mat.mean(axis=0, dtype=dtype)
 
 
-def normalized_mean(mat):
+def normalized_mean(mat, dtype=None):
     normalized_mat = normalize(mat)
-    return normalized_mat.mean(axis=0)
+    return normalized_mat.mean(axis=0, dtype=dtype)
 
 
 def apply_vec(func, vec_getter, stream, lang=None, **extra):
