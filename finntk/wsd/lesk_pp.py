07594cfad1732f2c37c3f26947af15802ffb32c2
Frankie Robertson <frankie@robertson.name>
2019-11-11 14:09:33 +0200

    Add some logging to Lesk++

--- finntk/wsd/lesk_pp.py
+++ finntk/wsd/lesk_pp.py
@@ -4,6 +4,10 @@ Implementation of Lesk++, which *may* be broken.
 from finntk.emb.autoextend import vecs as fiwn_vecs, mk_lemma_vec
 import numpy as np
 from .lesk_emb import MultilingualLesk
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class LeskPP:
@@ -24,11 +28,14 @@ class LeskPP:
         words = []
         vecs = []
         for idx, (lemma_str, lemmas) in enumerate(sent_lemmas):
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug(f"#{idx}: {lemma_str}")
             if exclude_idx is not None and idx == exclude_idx:
                 continue
             vec_appended = False
             if len(lemmas) == 1:
                 try:
+                    logger.debug("Used wn lemma vec")
                     vecs.append(mk_lemma_vec(lemmas[0]))
                 except KeyError:
                     pass
@@ -36,6 +43,7 @@ class LeskPP:
                     vec_appended = True
             if not vec_appended:
                 try:
+                    logger.debug("Used surface vec")
                     vecs.append(fiwn_space.get_vector(lemma_str))
                 except KeyError:
                     pass
@@ -44,9 +52,15 @@ class LeskPP:
             if vec_appended:
                 words.append(lemma_str)
 
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f"Used words: {words}")
+            logger.debug(f"Got vecs: {vecs}")
+
         if not vecs:
+            logger.debug(f"Early return!")
             return
         mat = np.stack(vecs)
+        logger.debug(f"Mat: {mat}")
         if hasattr(self.aggf, "needs_words"):
             return self.aggf(mat, words, "fi")
         else:
