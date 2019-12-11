b3e90dc58faaa22fda7295f1d4daeb63500c1df8
Frankie Robertson <frankie@robertson.name>
2019-12-11 09:48:43 +0200

    Add Wiktionary/WordNet POS map

---
+++ finntk/data/wordnet_pos.py
@@ -0,0 +1,16 @@
+POS_MAP = {
+    "v": ["Verb"],
+    "n": [
+        "Noun",
+        "Proper noun",
+        "Letter",
+        "Symbol",
+        "Abbreviation",
+        "Pronoun",
+        "Initialism",
+        "Contraction",
+        "Acronym",
+    ],
+    "a": ["Adjective", "Number", "Numeral", "Participle", "Ordinal number"],
+    "r": ["Adverb"],
+}
